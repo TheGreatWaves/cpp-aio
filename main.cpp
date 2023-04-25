1cebd099aebf0157c0e8324da767032df034e679
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-25 09:46:16 +0700

    Added logger and variable debugger

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-// All stdlib
+/* All stdlib */
 #include <bits/stdc++.h>
 
-int main()
-{
-  std::cout << "Hello world!" << '\n';
-}
+/* Types */
+using u8 = uint8_t;
+
+/* STRING MACRO HELPER */
+#define MAKE_STR(s) #s
+
+/* LOGGER */
+template<typename ... Args> auto log(Args&& ... args)->void{(std::cout<<...<<std::forward<Args>(args));} 
+
+/* VARIABLE DEBUGGER */
+#define DEBUG(variable) log("[Line: ",__LINE__,"] ",MAKE_STR(variable)," = ",variable,'\n'); 
+
+int main() {}
