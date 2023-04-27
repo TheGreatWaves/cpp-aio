8edafa988fe1dcd704da241833a9a4aaffa312b6
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-27 23:33:50 +0700

    Structs can now be debugged

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ template<typename ... Args> auto log(Args&& ... args)->void{(std::cout<<...<<std
 struct struct_name { \
   FIELDS(FIELD) \
   const char* name { #struct_name }; \
-  std::string stringify() \
+  std::string stringify() const \
   { \
     std::stringstream ss; \
     ss << "struct " << STR(struct_name) << "\n{\n"; \
@@ -29,16 +29,35 @@ struct struct_name { \
     ss << "}\n"; \
     return ss.str(); \
   } \
+  friend std::ostream& operator<<(std::ostream& os, const struct_name & obj) \
+  { \
+    os << obj.stringify(); \
+    return os; \
+  } \
 } 
+
+// src: https://www.scs.stanford.edu/~dm/blog/va-opt.html#the-for_each-macro
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+
+#define FIELDS(x) \
+  x(int, a); \
+  x(int, b);
+STRUCT(Something);
+
+
 
 int main() 
 {
-  #define FIELDS(x) \
-    x(int, a); \
-    x(int, b);
-  STRUCT(Something);
-
   auto smth = Something { 10, 2 };
-
-  log(smth.stringify());
+  DEBUG(smth);
+  smth.a = 22;
+  DEBUG(smth);
 }
